86096f4db6fb8ad17e027f95d3c610cb9e1c22b4
tortakoon222 <145433724+tortakoon222@users.noreply.github.com>
2023-09-26 15:05:51 +0700

    Update mystreamlit.py

--- mystreamlit.py
+++ mystreamlit.py
@@ -1,12 +1,12 @@
 import streamlit as st
 
 st.image('./pic/011.jpg')
-col1,col2 = st.colume(2)
+col1,col2 = st.columes(2)
 
 with col1:
     st.header('ต่อตระกูล สุขปาน')
 with col2:
     st.subheader('วิทยาการข้มูล')
-    st.text('๕ระวิทยาศาสตร์และเทคโนโลยี')
+    st.text('คณะวิทยาศาสตร์และเทคโนโลยี')
 
 st.header('Tortakoon Sukpan')
