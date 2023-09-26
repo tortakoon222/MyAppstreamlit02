23a4518024d82db230c755a9864de2ba496d7c1c
tortakoon222 <145433724+tortakoon222@users.noreply.github.com>
2023-09-26 15:02:36 +0700

    Update mystreamlit.py

--- mystreamlit.py
+++ mystreamlit.py
@@ -1,3 +1,11 @@
 import streamlit as st
 st.image('./pic/011.jpg')
+col1,col2 = st.colume(2)
+
+with col1:
+    st.header('ต่อตระกูล สุขปาน')
+with col2:
+    st.subheader('วิทยาการข้มูล')
+    st.text('๕ระวิทยาศาสตร์และเทคโนโลยี')
+
 st.header('Tortakoon Sukpan')
